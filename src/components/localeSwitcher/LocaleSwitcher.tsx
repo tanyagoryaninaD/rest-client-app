5d465b64ce218859981c912e337c4a57f82aaedc
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-03 15:59:05 +0300

    refactor: change select to a button with a drop-down list

--- src/components/localeSwitcher/LocaleSwitcher.tsx
+++ src/components/localeSwitcher/LocaleSwitcher.tsx
@@ -1,39 +1,105 @@
 'use client';
 
 import * as React from 'react';
-import { useLocale, useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 import { usePathname, Link } from '../../i18n/navigation';
-import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
 import '@/components/localeSwitcher/LocaleSwitcher.css';
+import LanguageIcon from '@mui/icons-material/Language';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Grow from '@mui/material/Grow';
+import Paper from '@mui/material/Paper';
+import Popper from '@mui/material/Popper';
+import MenuList from '@mui/material/MenuList';
 
 export default function LocaleSwitcher() {
-  const t = useTranslations('header');
+  const t = useTranslations('languages');
   const pathname = usePathname();
-  const locale = useLocale();
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLDivElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
-    <FormControl variant="standard" sx={{ minWidth: 120 }}>
-      <InputLabel id="locale-select-label">{t('languages.title')}</InputLabel>
-      <Select
-        labelId="locale-select-label"
-        id="locale-select"
-        label={t('languages.title')}
-        defaultValue={locale}
+    <React.Fragment>
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="Button group with a nested menu"
       >
-        <MenuItem value="en">
-          <Link href={pathname} locale="en" className="local-link">
-            {t('languages.en')}
-          </Link>
-        </MenuItem>
-        <MenuItem value="ru">
-          <Link href={pathname} locale="ru" className="local-link">
-            {t('languages.ru')}
-          </Link>
-        </MenuItem>
-      </Select>
-    </FormControl>
+        <Button
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+          sx={{
+            color: 'var(--background)',
+            backgroundColor: 'var(--foreground)',
+          }}
+        >
+          <LanguageIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{ zIndex: 1 }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  <MenuItem value="en">
+                    <Link
+                      href={pathname}
+                      locale="en"
+                      className="local-link"
+                    ></Link>
+                    {t('en')}
+                  </MenuItem>
+                  <MenuItem value="ru">
+                    <Link
+                      href={pathname}
+                      locale="ru"
+                      className="local-link"
+                    ></Link>
+
+                    {t('ru')}
+                  </MenuItem>
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </React.Fragment>
   );
 }
