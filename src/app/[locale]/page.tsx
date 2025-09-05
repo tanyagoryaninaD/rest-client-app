629050c4b88ef7dd74d989058130f0503ef9682b
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-09-05 15:02:42 +0300

    feat: update root layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,24 +1,60 @@
-import Image from 'next/image';
+'use client';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
-import LocaleSwitcher from '@/components/localeSwitcher/LocaleSwitcher';
 import { Link } from '@/i18n/navigation';
 
 export default function Home() {
+  // TODO: Add user context
+  const user = Math.random() > 0.5;
+
   return (
-    <div className="root">
-      <main>
-        <LocaleSwitcher />
-        <Link href={'/'}>
-          <Image src="/logo.svg" alt="Logo" width={30} height={30} priority />
-        </Link>
-        <Link href={'/sign-up'}>
-          <p>Sign Up</p>
-        </Link>
-        <Link href={'/sign-in'}>
-          <p>Sign In</p>
-        </Link>
-      </main>
-      <footer></footer>
-    </div>
+    <Container>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {user ? (
+          <>
+            <Typography variant="h3" component="h1" gutterBottom>
+              Welcome Back, [Username]!
+            </Typography>
+            <Stack spacing={2} direction="row" sx={{ mt: 6 }}>
+              <Button variant="contained" component={Link} href={'/client'}>
+                REST Client
+              </Button>
+              <Button variant="contained" component={Link} href={'/history'}>
+                History
+              </Button>
+              <Button variant="contained" component={Link} href={'/variables'}>
+                Variables
+              </Button>
+            </Stack>
+          </>
+        ) : (
+          <>
+            <Typography variant="h3" component="h1" gutterBottom>
+              Welcome!
+            </Typography>
+            <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+              <Button variant="contained" component={Link} href={'/sign-in'}>
+                Sign In
+              </Button>
+              <Button variant="outlined" component={Link} href={'/sign-up'}>
+                Sign Up
+              </Button>
+            </Stack>
+          </>
+        )}
+      </Box>
+    </Container>
   );
 }
