fe65093c4e870e760392a11d8988246f58d52bcc
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-09-05 20:09:02 +0300

    feat: separate sidebar component

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,10 +1,6 @@
 'use client';
-import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
 import MuiLink from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
@@ -14,6 +10,7 @@ import { useState } from 'react';
 import LocaleSwitcher from '@/components/localeSwitcher/LocaleSwitcher';
 import { Link, usePathname } from '@/i18n/navigation';
 
+import Sidebar from '../sidebar/sidebar';
 import AuthPanel from './auth-panel/auth-panel';
 import HeaderIconButton from './header-icon-button/header-icon-button';
 
@@ -67,40 +64,12 @@ export default function Header() {
           </HeaderIconButton>
         </Stack>
       </Toolbar>
-
-      <Drawer
-        anchor="right"
-        inert={!isSidebarOpen}
-        open={isSidebarOpen}
-        onClose={toggleSidebar}
-        sx={{
-          '& .MuiDrawer-paper': {
-            width: {
-              xs: '100%',
-              sm: 250,
-            },
-            minWidth: '40px',
-          },
-        }}
-      >
-        <Box sx={{ padding: 4, alignSelf: 'flex-end' }}>
-          <HeaderIconButton handleClick={toggleSidebar}>
-            <CloseIcon />
-          </HeaderIconButton>
-        </Box>
-
-        <Divider variant="middle" />
-
-        <Box sx={{ padding: 4, flexGrow: 1 }}>
-          <Box>
-            <AuthPanel
-              isSidebarOpen={isSidebarOpen}
-              user={user}
-              closeSidebar={closeSidebar}
-            />
-          </Box>
-        </Box>
-      </Drawer>
+      <Sidebar
+        isSidebarOpen={isSidebarOpen}
+        toggleSidebar={toggleSidebar}
+        closeSidebar={closeSidebar}
+        user={user}
+      />
     </AppBar>
   );
 }
