03cf4963b9b8b7a5ddbccd0f92e211a648186287
Alexander Misyura <87127837+AlexanderMisyura@users.noreply.github.com>
2025-09-10 15:47:10 +0300

    refactor: use 'useIsLoggedIn' hook in root page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -8,13 +8,13 @@ import { useTranslations } from 'next-intl';
 import Loader from '@/components/layout/loader/loader';
 import { AUTH_LINKS, CLIENT_LINKS } from '@/constants/links';
 import { useAppSelector } from '@/hooks/redux';
+import { useIsLoggedIn } from '@/hooks/use-is-logged-in';
 import { Link } from '@/i18n/navigation';
-import { isTokenValid } from '@/utils/firebase/tokenValidation';
 
 export default function Home() {
   const t = useTranslations('home_general');
   const { user, loading } = useAppSelector((state) => state.user);
-  const isLoggedIn = Boolean(user) && isTokenValid(user?.expiresIn);
+  const isLoggedIn = useIsLoggedIn();
 
   if (loading) {
     return <Loader />;
