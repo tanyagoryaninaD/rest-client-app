03cbadf590056d012cf57a6546aa1b12abac74a0
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-11 16:50:02 +0300

    refactor: rename file and impove contants

---
+++ src/constants/rest-client.ts
@@ -0,0 +1,40 @@
+export const HEADERS = {
+  'Content-Type': [
+    'application/json',
+    'text/html',
+    'application/xml',
+    'multipart/form-data',
+    'application/x-www-form-urlencoded',
+  ],
+  Accept: ['application/json', 'text/html', 'application/xml', '*/*'],
+  Authorization: ['Bearer <token>', 'Basic <credentials>'],
+  'Cache-Control': [],
+  'Accept-Language': ['en-US', 'ru-RU'],
+  'User-Agent': [],
+  Cookie: ['sessionId=', 'user='],
+  'X-Requested-With': ['XMLHttpRequest'],
+  'Accept-Encoding': [],
+  'Access-Control-Allow-Origin': [],
+  'Access-Control-Allow-Methods': [],
+  'Access-Control-Allow-Headers': [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+  ],
+  'Content-Disposition': [],
+  'Content-Security-Policy': [],
+  'Referrer-Policy': [],
+  'Strict-Transport-Security': [],
+  'X-Frame-Options': [],
+  'X-Content-Type-Options': [],
+  ETag: [],
+  'If-None-Match': [],
+  'Last-Modified': [],
+} as const;
+
+export const METHODS = ['get', 'post', 'put', 'patch', 'delete'] as const;
+
+export const CLIENT_FORM = {
+  method: 'method',
+  url: 'url',
+} as const;
