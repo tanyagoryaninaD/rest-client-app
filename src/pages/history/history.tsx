fdcd510b878d389a76569750128e683cc34f7a7d
DenisNedelko <dnsndlk@gmail.com>
2025-09-14 12:38:46 +0300

    feat: add layout to cards 2025-09-14 12:38:45

--- src/pages/history/history.tsx
+++ src/pages/history/history.tsx
@@ -2,7 +2,10 @@
 
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { useEffect, useState } from 'react';
 
@@ -67,11 +70,105 @@ export default function History() {
           </Button>
         </>
       ) : (
-        <Box>
+        <Stack
+          spacing={2}
+          sx={{
+            mt: 3,
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           {requests.map((item) => (
-            <Typography key={item.id}>{JSON.stringify(item)}</Typography>
+            <CardActionArea
+              key={item.id}
+              component={Link}
+              href={`/client/${item.id}`}
+              sx={{ display: 'block', maxWidth: 800 }}
+            >
+              <Card
+                variant="outlined"
+                sx={{
+                  borderRadius: 2,
+                  p: 1,
+                  display: 'flex',
+                  flexDirection: 'column',
+
+                  '&:hover': {
+                    borderColor: 'primary.main',
+                    boxShadow: 2,
+                  },
+                }}
+              >
+                <Typography
+                  sx={{
+                    alignSelf: 'flex-end',
+                    fontSize: '0.75rem',
+                  }}
+                  variant="body1"
+                  color="text.secondary"
+                >
+                  {new Date(item.requestTimestamp).toLocaleString()}
+                </Typography>
+                <Typography
+                  sx={{ alignSelf: 'flex-start', mb: 1 }}
+                  variant="h6"
+                >
+                  {item.requestMethod}
+                </Typography>
+                <Typography
+                  sx={{ alignSelf: 'flex-start', mb: 1 }}
+                  variant="body1"
+                >
+                  {item.endpointUrl}
+                </Typography>
+
+                <Box
+                  sx={{
+                    display: 'flex',
+                    borderTop: 1,
+                    borderColor: 'divider',
+                  }}
+                >
+                  {[
+                    { label: 'Status', value: item.responseStatusCode },
+                    { label: 'Latency', value: `${item.requestDuration}ms` },
+                    { label: 'Request Size', value: `${item.requestSize}B` },
+                    { label: 'Response Size', value: `${item.responseSize}B` },
+                  ].map((field) => (
+                    <Box
+                      key={field.label}
+                      sx={{
+                        flex: 1,
+                        textAlign: 'center',
+                        py: 1,
+                        borderColor: 'divider',
+                        display: 'flex',
+                        flexDirection: 'column',
+                      }}
+                    >
+                      <Typography variant="caption" color="text.secondary">
+                        {field.label}
+                      </Typography>
+                      <Typography variant="body2">{field.value}</Typography>
+                    </Box>
+                  ))}
+                </Box>
+
+                {item.errorDetails && (
+                  <Typography
+                    variant="body2"
+                    color="error"
+                    sx={{ mt: 1, textAlign: 'center' }}
+                  >
+                    {item.errorDetails}
+                  </Typography>
+                )}
+              </Card>
+            </CardActionArea>
           ))}
-        </Box>
+        </Stack>
       )}
     </Container>
   );
