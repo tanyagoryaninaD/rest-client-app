abc0010c0ce7b5b0933fbabf1f286bd9d39c0a36
DenisNedelko <dnsndlk@gmail.com>
2025-09-14 14:29:27 +0300

    test: add tests and fixed method sort 2025-09-14 14:29:27

--- src/utils/firebase/sortHistoryCollection.ts
+++ src/utils/firebase/sortHistoryCollection.ts
@@ -2,6 +2,6 @@ import type { HistoryCollection } from '@/types/userData';
 
 export function sortHistoryCollection(collection: HistoryCollection[]) {
   return [...collection].sort(
-    (a, b) => a.requestTimestamp - b.requestTimestamp
+    (a, b) => b.requestTimestamp - a.requestTimestamp
   );
 }
