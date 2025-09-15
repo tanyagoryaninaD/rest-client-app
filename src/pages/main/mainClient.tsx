1ff21ebb0c21627abc2b866a8b2b45978f16813d
DenisNedelko <dnsndlk@gmail.com>
2025-09-15 14:35:46 +0300

    feat: moved the main page to SSR 2025-09-15 14:35:46

---
+++ src/pages/main/mainClient.tsx
@@ -0,0 +1,82 @@
+'use client';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { useTranslations } from 'next-intl';
+
+import { AUTH_LINKS, CLIENT_LINKS } from '@/constants/links';
+import { Link } from '@/i18n/navigation';
+
+interface HomePageClientProps {
+  currentUser: string;
+  isNewUser: boolean;
+}
+export default function HomePageClient({
+  currentUser,
+  isNewUser,
+}: HomePageClientProps) {
+  console.log(currentUser);
+  const t = useTranslations('home_general');
+
+  return (
+    <Container>
+      <Stack
+        direction="column"
+        sx={{
+          my: 4,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {currentUser ? (
+          <>
+            {currentUser ? (
+              <Typography variant="h4" component="h1" gutterBottom>
+                {isNewUser
+                  ? t('greetings_firstRegistered', { name: currentUser })
+                  : t('greetings_registered', { name: currentUser })}
+              </Typography>
+            ) : (
+              <Typography variant="h4" component="h1" gutterBottom>
+                {t('greetings_unregistered')}
+              </Typography>
+            )}
+            <Stack spacing={2} direction="row" sx={{ mt: 6 }}>
+              {CLIENT_LINKS.map((link) => (
+                <Button
+                  key={link}
+                  variant="contained"
+                  component={Link}
+                  href={`/${link}`}
+                  data-testid={`link-${link}`}
+                >
+                  {t(`buttons.${link}`)}
+                </Button>
+              ))}
+            </Stack>
+          </>
+        ) : (
+          <>
+            <Typography variant="h4" component="h1" gutterBottom>
+              {t('greetings_unregistered')}
+            </Typography>
+            <Stack spacing={2} direction="row" sx={{ mt: 4 }}>
+              {AUTH_LINKS.map((link) => (
+                <Button
+                  key={link}
+                  variant="contained"
+                  component={Link}
+                  href={`/${link}`}
+                  data-testid={`link-${link}`}
+                >
+                  {t(`buttons.${link}`)}
+                </Button>
+              ))}
+            </Stack>
+          </>
+        )}
+      </Stack>
+    </Container>
+  );
+}
