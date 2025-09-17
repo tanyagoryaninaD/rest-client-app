25e42a29b0b5a591e390ebbf1c017d6dd8337cdb
DenisNedelko <dnsndlk@gmail.com>
2025-09-17 20:27:48 +0300

    feat: replace variables in client form 2025-09-17 20:27:48

--- src/components/forms/rest-client/form.tsx
+++ src/components/forms/rest-client/form.tsx
@@ -11,11 +11,13 @@ import RestClientResponse from '@/components/forms/rest-client/response/rest-cli
 import Loader from '@/components/layout/loader/loader';
 import { REQUEST_ABORTED } from '@/constants/app';
 import { CLIENT_PROXY_ERRORS } from '@/constants/client-proxy-errors';
+import { useAppSelector } from '@/hooks/redux';
 import { usePathname, useRouter } from '@/i18n/navigation';
 import type {
   ClientFormStateProps,
   ClientResponseStateProps,
 } from '@/types/components/rest-client';
+import { changeVariables } from '@/utils/change-variable-in-field';
 import { utf8ToBase64 } from '@/utils/handlers/base64';
 import {
   createHeadersObject,
@@ -27,7 +29,7 @@ export default function FormRestClient() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
-
+  const { variables } = useAppSelector((state) => state.variables);
   const clientForm = useForm<ClientFormStateProps>({
     mode: 'onChange',
     defaultValues: parseURLtoFormData(pathname, searchParams),
@@ -129,17 +131,30 @@ export default function FormRestClient() {
   }, [pathname, searchParams, t]);
 
   const onSubmit: SubmitHandler<ClientFormStateProps> = (data): void => {
-    const basePathnames = pathname.split('/').slice(0, 2);
-    basePathnames.push(data.method, utf8ToBase64(data.url));
+    const submittedData = {
+      ...data,
+      url: changeVariables(data.url, variables),
+      body: data.body ? changeVariables(data.body, variables) : '',
+      headers: data.headers.map((item) => ({
+        key: changeVariables(item.key, variables),
+        value: changeVariables(item.value, variables),
+      })),
+    };
 
-    if (data.body) {
-      basePathnames.push(utf8ToBase64(data.body));
+    const basePathnames = pathname.split('/').slice(0, 2);
+    basePathnames.push(submittedData.method, utf8ToBase64(submittedData.url));
+
+    if (submittedData.body) {
+      basePathnames.push(utf8ToBase64(submittedData.body));
     }
 
     const newPathname = basePathnames.join('/');
     router.replace({
       pathname: newPathname,
-      query: data.headers.length > 0 ? createHeadersObject(data.headers) : {},
+      query:
+        submittedData.headers.length > 0
+          ? createHeadersObject(submittedData.headers)
+          : {},
     });
   };
 
