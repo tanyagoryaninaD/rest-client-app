15d45be97cd787ca8a8b938f50766c1cc488222d
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-18 03:13:31 +0300

    refactor: improve generator responsiveness

--- src/components/forms/rest-client/request/generate-code/result.tsx
+++ src/components/forms/rest-client/request/generate-code/result.tsx
@@ -1,19 +1,25 @@
 import { TextField } from '@mui/material';
 import { useTranslations } from 'next-intl';
+import { useDeferredValue } from 'react';
 
+import Loading from '@/components/layout/loader/loader';
 import type { GenerateCodeResultProps } from '@/types/components/rest-client';
 
 export default function GenerateCodeResult(props: GenerateCodeResultProps) {
   const t = useTranslations('rest-client.request');
   const { generatedCode, isPending } = props;
+  const deferredQuery = useDeferredValue(generatedCode);
 
-  return (
+  return isPending && !generatedCode ? (
+    <Loading />
+  ) : (
     <TextField
       sx={{ width: '100%' }}
       multiline
       fullWidth
       label={t('labels.snippet')}
-      value={isPending ? '' : generatedCode}
+      value={deferredQuery}
+      data-testid="request-generator-snippet"
       slotProps={{
         input: {
           sx: { fontFamily: 'monospace' },
