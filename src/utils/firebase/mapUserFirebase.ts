75dd81896ec29f05ad14014c434c4fdaa12639e3
DenisNedelko <dnsndlk@gmail.com>
2025-09-18 17:13:48 +0300

    fix: convert expiries time 2025-09-18 17:13:48

--- src/utils/firebase/mapUserFirebase.ts
+++ src/utils/firebase/mapUserFirebase.ts
@@ -6,7 +6,8 @@ export const mapUserFirebase = async (user: User) => {
   const expiresIn = await getExpirationTime(user);
   const userId = user.uid;
   const token = await user.getIdToken();
-  document.cookie = `token=${token}; path=/; max-age=${expiresIn}`;
+  const maxAge = Math.floor((expiresIn - Date.now()) / 1000);
+  document.cookie = `token=${token}; path=/; max-age=${maxAge}`;
   return {
     userId,
     displayName: user.displayName,
--- src/utils/firebase/tokenValidation.ts
+++ src/utils/firebase/tokenValidation.ts
@@ -6,7 +6,7 @@ export const getExpirationTime = async (user: User | null) => {
   }
   const idToken = await user.getIdTokenResult();
   const expiresIn = new Date(idToken.expirationTime).getTime();
-  return Math.floor((expiresIn - Date.now()) / 1000);
+  return expiresIn;
 };
 
 export const isTokenValid = (expiresIn?: number): boolean => {
