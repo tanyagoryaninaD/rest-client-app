ba0fdfb0083a884bb8bd5088372c8e49f6eb0838
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-18 03:23:15 +0300

    refactor: remove unnecessary code

--- src/components/forms/rest-client/request/generate-code/result.tsx
+++ src/components/forms/rest-client/request/generate-code/result.tsx
@@ -1,6 +1,5 @@
 import { TextField } from '@mui/material';
 import { useTranslations } from 'next-intl';
-import { useDeferredValue } from 'react';
 
 import Loading from '@/components/layout/loader/loader';
 import type { GenerateCodeResultProps } from '@/types/components/rest-client';
@@ -8,7 +7,6 @@ import type { GenerateCodeResultProps } from '@/types/components/rest-client';
 export default function GenerateCodeResult(props: GenerateCodeResultProps) {
   const t = useTranslations('rest-client.request');
   const { generatedCode, isPending } = props;
-  const deferredQuery = useDeferredValue(generatedCode);
 
   return isPending && !generatedCode ? (
     <Loading />
@@ -18,7 +16,7 @@ export default function GenerateCodeResult(props: GenerateCodeResultProps) {
       multiline
       fullWidth
       label={t('labels.snippet')}
-      value={deferredQuery}
+      value={generatedCode}
       data-testid="request-generator-snippet"
       slotProps={{
         input: {
